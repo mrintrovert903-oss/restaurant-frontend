889d6d73e4d257e2631ce7354de5ac0a0d322061
Alwayshari1 <gupteswarraohari0000@gmail.com>
2026-01-14 21:47:38 +0530

    local

--- src/api/api.js
+++ src/api/api.js
@@ -11,10 +11,29 @@
 // });
 
 // export default API;
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
+
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 API.interceptors.request.use(
